d72dfa492752b04e9321057683af25e91ac6c5b3
simorra <simorra95@gmail.com>
2022-02-15 16:20:34 +0100

    Implement input handling

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 import { Vec2d } from "./Vec2d";
+import { CircularQueue } from "./CircularQueue";
 
 const TILE_SIZE = 10;
 
@@ -12,12 +13,42 @@ let snake = {
 };
 let targets = []; //target tile for each piece of the snake's body
 
+let inputBuffer = new CircularQueue(10);
+function keydownHandler(ev) {
+  //Ignore held down keys
+  if(ev.repeat)
+    return;
+
+  let last = inputBuffer.peekLast(); //last input received
+  switch(ev.code) {
+    case "ArrowUp":
+      //Save the new input only if it's meaningful (same in the following cases)
+      if(last !== "ArrowUp" && last !== "ArrowDown")
+        inputBuffer.enqueue("ArrowUp");
+      break;
+    case "ArrowRight":
+      if(last !== "ArrowRight" && last !== "ArrowLeft")
+        inputBuffer.enqueue("ArrowRight");
+      break;
+    case "ArrowDown":
+      if(last !== "ArrowDown" && last !== "ArrowUp")
+        inputBuffer.enqueue("ArrowDown");
+      break;
+    case "ArrowLeft":
+      if(last !== "ArrowLeft" && last !== "ArrowRight")
+        inputBuffer.enqueue("ArrowLeft");
+      break;
+    default:
+  }
+}
+
 window.onload = init;
 
 function init() {
   canvas = document.querySelector("#gameBoard");
   gridWidth = Math.floor(canvas.width / TILE_SIZE);
   gridHeight = Math.floor(canvas.height / TILE_SIZE);
+  canvas.addEventListener("keydown", keydownHandler);
 
   //snake.body[0] contains the position of the snake's head
   snake.body[0] = new Vec2d(Math.floor(gridWidth/2), Math.floor(gridHeight/2)); 
